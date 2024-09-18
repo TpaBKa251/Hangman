7cd1ac1e99f47832918dab1964c68809d1d68bc6
Лапшин Илья Романович <ilia.lapshin2016@yandex.ru>
2024-09-18 19:49:31 +0700

    docs(difficulty): добавлены JavaDoc для перечисления DifficultyEnum

--- src/main/java/backend/academy/hangman/enums/DifficultyEnum.java
+++ src/main/java/backend/academy/hangman/enums/DifficultyEnum.java
@@ -2,23 +2,26 @@ package backend.academy.hangman.enums;
 
 import lombok.RequiredArgsConstructor;
 
+/**
+ * Перечисление со сложностями игры
+ */
 @RequiredArgsConstructor
-
 public enum DifficultyEnum {
     EASY("ЛЕГКАЯ"),
     MEDIUM("СРЕДНЯЯ"),
     HARD("ВЫСОКАЯ");
 
+    /**
+     * Имя сложности на русском языке
+     */
     private final String nameDifficulty;
 
+    /**
+     * Переопределенный метод {@code toString()}
+     * @return имя сложности на русском языке
+     */
     @Override
     public String toString() {
         return nameDifficulty;
     }
 }
-
-/*
-feat: добавлено перечисление DifficultyEnum
-
-Добавлено перечисление DifficultyEnum, которое содержит уровни сложности игры.
- */
