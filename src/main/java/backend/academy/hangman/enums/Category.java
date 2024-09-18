22ec37047434e814c8ab844e9ec64c30a1c9db47
Лапшин Илья Романович <ilia.lapshin2016@yandex.ru>
2024-09-18 19:49:46 +0700

    docs: добавлены JavaDoc для перечисления Category

--- src/main/java/backend/academy/hangman/enums/Category.java
+++ src/main/java/backend/academy/hangman/enums/Category.java
@@ -2,21 +2,25 @@ package backend.academy.hangman.enums;
 
 import lombok.RequiredArgsConstructor;
 
+/**
+ * Перечисление с категориями слов
+ */
 @RequiredArgsConstructor
 public enum Category {
     ANIMALS("ЖИВОТНЫЕ"),
     FILMS("ФИЛЬМЫ");
 
+    /**
+     * Имя категории на русском языке
+     */
     private final String nameCategory;
 
+    /**
+     * Переопределенный метод {@code toString()}
+     * @return имя категории на русском языке
+     */
     @Override
     public String toString() {
         return nameCategory;
     }
 }
-
-/*
-feat: добавлено перечисление Category
-
-Добавлено перечисление Category, которое содержит категории слов.
- */
