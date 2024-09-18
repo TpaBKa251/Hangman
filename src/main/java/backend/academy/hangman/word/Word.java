f122eb0277a96ef0e12c69f49c38dc2e80c87717
Лапшин Илья Романович <ilia.lapshin2016@yandex.ru>
2024-09-18 18:52:39 +0700

    feat(word): добавлен record Word для хранения слова и подсказки к нему
    Добавлен новый record Word с string полями для слова и подсказки к слову

---
+++ src/main/java/backend/academy/hangman/word/Word.java
@@ -0,0 +1,10 @@
+package backend.academy.hangman.word;
+
+public record Word(String word, String help) {
+}
+
+/*
+feat: добавлен класс Word
+
+Добавлен класс Word для хранения слов и подсказок к ним.
+ */
