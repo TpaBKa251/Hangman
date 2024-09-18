b27ee781b8c8b396cea67ef545baf1637f220097
Лапшин Илья Романович <ilia.lapshin2016@yandex.ru>
2024-09-18 19:46:13 +0700

    docs(word): добавлены JavaDoc для record Word

--- src/main/java/backend/academy/hangman/word/Word.java
+++ src/main/java/backend/academy/hangman/word/Word.java
@@ -1,10 +1,9 @@
 package backend.academy.hangman.word;
 
+/**
+ * Record для хранения слова и подсказки к нему
+ * @param word слово
+ * @param help подсказка
+ */
 public record Word(String word, String help) {
 }
-
-/*
-feat: добавлен класс Word
-
-Добавлен класс Word для хранения слов и подсказок к ним.
- */
