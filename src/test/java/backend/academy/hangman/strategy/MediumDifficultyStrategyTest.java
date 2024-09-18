a023e1cbaca60d45805edd975ceaba4de424041c
Лапшин Илья Романович <ilia.lapshin2016@yandex.ru>
2024-09-18 21:36:28 +0700

    test(difficulty): добавлены unit-тесты MediumDifficultyStrategyTest для класса MediumDifficultyStrategy
    Протестирована корректность работы всех методов класса MediumDifficultyStrategy.
    
    Тесты включают:
    - тестирование корректности получения максимального количества попыток, сложности, паттерна и сообщения об ошибке валидации;
    - тестирование корректного вызова метода getWord() в классе WordManager;
    - тестирование корректного вызова метода print() в классе VisualizerHangman.

---
+++ src/test/java/backend/academy/hangman/strategy/MediumDifficultyStrategyTest.java
@@ -0,0 +1,84 @@
+package backend.academy.hangman.strategy;
+
+import backend.academy.hangman.enums.Category;
+import backend.academy.hangman.enums.DifficultyEnum;
+import backend.academy.hangman.output.VisualizerHangman;
+import backend.academy.hangman.word.WordManager;
+import java.util.regex.Pattern;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+public class MediumDifficultyStrategyTest {
+
+    @Mock
+    private WordManager wordManager;
+
+    @Mock
+    private VisualizerHangman visualizerHangman;
+
+    @InjectMocks
+    private MediumDifficultyStrategy strategy;
+
+    @Test
+    public void testGetWord() {
+        DifficultyEnum expectedDifficulty = DifficultyEnum.MEDIUM;
+        Category expectedCategory = Category.ANIMALS;
+
+        strategy.getWord(Category.ANIMALS);
+
+        verify(wordManager).getWord(expectedDifficulty, expectedCategory);
+    }
+
+    @Test
+    public void testGetMaxAttempts() {
+        int expectedMaxAttempts = 8;
+
+        int actualMaxAttempts = strategy.getMaxAttempts();
+
+        assertEquals(expectedMaxAttempts, actualMaxAttempts);
+    }
+
+    @Test
+    public void testGetDifficulty() {
+        DifficultyEnum expectedDifficulty = DifficultyEnum.MEDIUM;
+
+        DifficultyEnum actualDifficultyEnum = strategy.getDifficulty();
+
+        assertEquals(expectedDifficulty, actualDifficultyEnum);
+    }
+
+    @Test
+    public void testPrintGallows() {
+        int currAttempts = 2;
+        int maxAttempts = strategy.getMaxAttempts();
+        int expectedAttempts = currAttempts + (DifficultyStrategy.MAX_ATTEMPTS - maxAttempts);
+
+        strategy.printGallows(currAttempts);
+
+        verify(visualizerHangman).print(expectedAttempts);
+    }
+
+    @Test
+    public void testGetPattern() {
+        String expectedPattern = Pattern.compile("[А-Яа-я]").pattern();
+
+        String actualPattern = strategy.getPattern().pattern();
+
+        assertEquals(expectedPattern, actualPattern);
+    }
+
+    @Test
+    public void testGetErrorValidMessage() {
+        String expectedMessage = "Вводите по одной букве русского алфавита!";
+
+        String actualMessage = strategy.getErrorValidMessage();
+
+        assertEquals(expectedMessage, actualMessage);
+    }
+}
