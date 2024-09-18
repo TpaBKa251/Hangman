bc4ff4ce0e3b93a30e638831be5f8068056d499a
Лапшин Илья Романович <ilia.lapshin2016@yandex.ru>
2024-09-18 20:08:56 +0700

    test(word): добавлены unit-тесты WordManagerTest для класса WordManager
    Добавлены unit-тесты WordManagerTest для класса WordManager, которые тестируют корректность выбора слова.
    
    Тесты включают:
    - тестирование выбора слова легкой сложности категории животные;
    - тестирование выбора слова средней сложности категории фильмы;
    - тестирование выбора слова высокой сложности категории животные.

---
+++ src/test/java/backend/academy/hangman/word/WordManagerTest.java
@@ -0,0 +1,66 @@
+package backend.academy.hangman.word;
+
+import backend.academy.hangman.enums.Category;
+import backend.academy.hangman.enums.DifficultyEnum;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.security.SecureRandom;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class WordManagerTest {
+
+    @Mock
+    private SecureRandom random;
+
+    @InjectMocks
+    private WordManager wordManager;
+
+    @Test
+    public void testEasyAnimals() {
+        Word expectedWord = new Word("КОТ", "Самое популярное домашнее животное");
+        DifficultyEnum difficultyEnum = DifficultyEnum.EASY;
+        Category category = Category.ANIMALS;
+
+        when(random.nextInt(3)).thenReturn(0);
+
+        Word actualWord = wordManager.getWord(difficultyEnum, category);
+
+        assertEquals(expectedWord.word(), actualWord.word());
+        assertEquals(expectedWord.help(), actualWord.help());
+    }
+
+    @Test
+    public void testMediumFilms() {
+        Word expectedWord = new Word("ТЕРМИНАТОР", "Фильм про борьбу человечества с машинами");
+        DifficultyEnum difficultyEnum = DifficultyEnum.MEDIUM;
+        Category category = Category.FILMS;
+
+        when(random.nextInt(3)).thenReturn(1);
+
+        Word actualWord = wordManager.getWord(difficultyEnum, category);
+
+        assertEquals(expectedWord.word(), actualWord.word());
+        assertEquals(expectedWord.help(), actualWord.help());
+    }
+
+    @Test
+    public void testHardAnimals() {
+        Word expectedWord = new Word("АВСТРАЛИЙСКАЯ ЕХИДНА", "Перевернутый антагонист Соника");
+        DifficultyEnum difficultyEnum = DifficultyEnum.HARD;
+        Category category = Category.ANIMALS;
+
+        when(random.nextInt(3)).thenReturn(2);
+
+        Word actualWord = wordManager.getWord(difficultyEnum, category);
+
+        assertEquals(expectedWord.word(), actualWord.word());
+        assertEquals(expectedWord.help(), actualWord.help());
+    }
+}
