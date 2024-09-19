fe0609fbbd3c1f7773e55e40d7eda49c16e07b7b
Лапшин Илья Романович <ilia.lapshin2016@yandex.ru>
2024-09-20 01:06:29 +0700

    style: удален неиспользуемый импорт в классе GameTest
    Удален неиспользуемый импорт org.mockito.Mockito.mock в классе GameTest

--- src/test/java/backend/academy/hangman/GameTest.java
+++ src/test/java/backend/academy/hangman/GameTest.java
@@ -11,7 +11,6 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import static org.mockito.Mockito.inOrder;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
