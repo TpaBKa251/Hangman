8f5383c5bf5aa79150aedd3c641de68384bf431c
Лапшин Илья Романович <ilia.lapshin2016@yandex.ru>
2024-09-20 00:15:40 +0700

    feat(fabric): добавлен новый класс GameStatesFabric для реализации фабрики состояний
    Добавлен новый класс фабрики состояний GameStatesFabric, создающий экземпляры классов состояний. Нужный экземпляр определяется элементом перечисления GameStates

---
+++ src/main/java/backend/academy/hangman/fabric/GameStatesFabric.java
@@ -0,0 +1,29 @@
+package backend.academy.hangman.fabric;
+
+import backend.academy.hangman.enums.GameStates;
+import backend.academy.hangman.input.Reader;
+import backend.academy.hangman.setting.GameConfigurator;
+import backend.academy.hangman.setting.PlayerPreferences;
+import backend.academy.hangman.state.GameBaseState;
+import backend.academy.hangman.state.impl.LoseState;
+import backend.academy.hangman.state.impl.PlayingState;
+import backend.academy.hangman.state.impl.SettingsState;
+import backend.academy.hangman.state.impl.WinState;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class GameStatesFabric {
+    private final Reader reader;
+    private final PlayerPreferences playerPreferences;
+    private final GameConfigurator gameConfigurator;
+
+    public GameBaseState createGameState(GameStates gameState) {
+        return switch (gameState) {
+            case GameStates.SETTINGS -> new SettingsState(playerPreferences, gameConfigurator);
+            case PLAYING -> new PlayingState(reader);
+            case WIN -> new WinState(reader);
+            case LOSE -> new LoseState(reader);
+            default -> null;
+        };
+    }
+}
