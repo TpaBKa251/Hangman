730b1b2fab253f3b43372a1b05ee70d5bc59ae15
Лапшин Илья Романович <ilia.lapshin2016@yandex.ru>
2024-09-20 00:10:27 +0700

    feat(state): добавлен новый класс WinState для реализации состояния победы
    Добавлен новый класс WinState для реализации состояния победы, выводящий сообщение о победе.
    
    Может завершить игру или начать новую попытку (переключить в состояние настройки).

---
+++ src/main/java/backend/academy/hangman/state/impl/WinState.java
@@ -0,0 +1,51 @@
+package backend.academy.hangman.state.impl;
+
+import backend.academy.hangman.Game;
+import backend.academy.hangman.enums.GameStates;
+import backend.academy.hangman.input.Reader;
+import backend.academy.hangman.output.Interface;
+import backend.academy.hangman.state.GameBaseState;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class WinState implements GameBaseState {
+    private final Reader reader;
+
+    @Override
+    public void handle(Game game) {
+        Interface.print("""
+           ⠀⠀ ⠀⠀⠀⠀⠀⠀⠀⠀⠀ ⢰⣦⣤⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+           ⠀ ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀ ⣼⣿⣿⣿⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+            ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀ ⣠⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+            ⠀⠀⠀⠀⠀⠀⠀⠀ ⣠⣾⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+            ⢠⣤⣤⣤⡀⠀⢀⣼⣿⣿⣿⣿⣿⣿⣧⣤⣤⣤⣤⣤⣤⣤⣤⣄⡀⠀
+            ⢸⣿⣿⣿⡇⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡄
+            ⢸⣿⣿⣿⡇⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇
+            ⢸⣿⣿⣿⡇⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠀
+            ⢸⣿⣿⣿⡇⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠁⠀
+            ⢸⣿⣿⣿⡇⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠁⠀⠀
+            ⢸⣿⣿⣿⡇⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠃⠀⠀⠀
+            ⢸⣿⣿⣿⡇⠀⠈⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠃⠀⠀⠀⠀
+            __          _______ _   _
+            \\ \\        / /_   _| \\ | |
+             \\ \\  /\\  / /  | | |  \\| |
+              \\ \\/  \\/ /   | | | . ` |
+               \\  /\\  /   _| |_| |\\  |
+                \\/  \\/   |_____|_| \\_|
+
+            Начать новую игру?
+                1. Да
+                2. Нет""", true);
+    }
+
+    @Override
+    public GameStates nextState() {
+        String ans = reader.read();
+
+        if ("1".equals(ans)) {
+            return GameStates.SETTINGS;
+        }
+
+        return GameStates.EXIT;
+    }
+}
