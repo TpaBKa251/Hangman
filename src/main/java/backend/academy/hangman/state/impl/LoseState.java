f9fae0ed4e102193448beb4a2e5b8db313df32d4
Лапшин Илья Романович <ilia.lapshin2016@yandex.ru>
2024-09-20 00:13:00 +0700

    feat(state): добавлен новый класс LoseState для реализации состояния поражения
    Добавлен новый класс LoseState для реализации состояния поражения, выводящий сообщение о поражении.
    
    Может завершить игру или начать новую попытку (переключить в состояние настройки).

---
+++ src/main/java/backend/academy/hangman/state/impl/LoseState.java
@@ -0,0 +1,65 @@
+package backend.academy.hangman.state.impl;
+
+import backend.academy.hangman.Game;
+import backend.academy.hangman.enums.GameStates;
+import backend.academy.hangman.input.Reader;
+import backend.academy.hangman.output.Interface;
+import backend.academy.hangman.state.GameBaseState;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class LoseState implements GameBaseState {
+    private final Reader reader;
+
+    @Override
+    public void handle(Game game) {
+        Interface.print("""
+                             uu$$$$$$$$$$$uu
+                          uu$$$$$$$$$$$$$$$$$uu
+                         u$$$$$$$$$$$$$$$$$$$$$u
+                        u$$$$$$$$$$$$$$$$$$$$$$$u
+                       u$$$$$$$$$$$$$$$$$$$$$$$$$u
+                       u$$$$$$*   *$$$*   *$$$$$$u
+                       *$$$$*      u$u       $$$$*
+                        $$$u       u$u       u$$$
+                        $$$u      u$$$u      u$$$
+                         *$$$$uu$$$   $$$uu$$$$*
+                          *$$$$$$$*   *$$$$$$$*
+                            u$$$$$$$u$$$$$$$u
+                             u$*$*$*$*$*$*$u
+                  uuu        $$u$ $ $ $ $u$$       uuu
+                  u$$$$       $$$$$u$u$u$$$       u$$$$
+                  $$$$$uu      *$$$$$$$$$*     uu$$$$$$
+                u$$$$$$$$$$$uu    *****    uuuu$$$$$$$$$
+                $$$$***$$$$$$$$$$uuu   uu$$$$$$$$$***$$$*
+                 ***      **$$$$$$$$$$$uu **$***
+                          uuuu **$$$$$$$$$$uuu
+                 u$$$uuu$$$$$$$$$uu **$$$$$$$$$$$uuu$$$
+                 $$$$$$$$$$****           **$$$$$$$$$$$*
+                   *$$$$$*                      **$$$$**
+                     $$$*                         $$$$*
+                        _      ____   _____ ______
+                       | |    / __ \\ / ____|  ____|
+                       | |   | |  | | (___ | |__
+                       | |   | |  | |\\___ \\|  __|
+                       | |___| |__| |____) | |____
+                       |______\\____/|_____/|______|
+
+                Загаданное слово:""" + game.settings().word().word() + """
+
+                Начать новую игру?
+                    1. Да
+                    2. Нет""", true);
+    }
+
+    @Override
+    public GameStates nextState() {
+        String ans = reader.read();
+
+        if ("1".equals(ans)) {
+            return GameStates.SETTINGS;
+        }
+
+        return GameStates.EXIT;
+    }
+}
