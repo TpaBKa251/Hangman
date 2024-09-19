4439f25640cd29d6d0a4a0c463fb7b8a26a1a5e2
Лапшин Илья Романович <ilia.lapshin2016@yandex.ru>
2024-09-20 00:20:40 +0700

    test(state): добавлены unit-тесты GameStatesManagerTest для класса GameStatesManager
    Добавлены unit-тесты GameStatesManagerTest для класса GameStatesManager, тестирующие корректность инициализации начального состояния, переключения между состояниями

---
+++ src/test/java/backend/academy/hangman/state/GameStatesManagerTest.java
@@ -0,0 +1,97 @@
+package backend.academy.hangman.state;
+
+import backend.academy.hangman.Game;
+import backend.academy.hangman.enums.GameStates;
+import backend.academy.hangman.fabric.GameStatesFabric;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class GameStatesManagerTest {
+
+    @Mock
+    private GameStatesFabric gameStatesFabric;
+
+    @Mock
+    private GameBaseState settingsState;
+
+    @Mock
+    private GameBaseState playingState;
+
+    @Mock
+    private GameBaseState winState;
+
+    @Mock
+    private GameBaseState loseState;
+
+    @Mock
+    private Game game;
+
+    private GameStatesManager gameStatesManager;
+
+    @BeforeEach
+    public void setUp() {
+        when(gameStatesFabric.createGameState(GameStates.SETTINGS)).thenReturn(settingsState);
+        when(gameStatesFabric.createGameState(GameStates.PLAYING)).thenReturn(playingState);
+        when(gameStatesFabric.createGameState(GameStates.WIN)).thenReturn(winState);
+        when(gameStatesFabric.createGameState(GameStates.LOSE)).thenReturn(loseState);
+
+        gameStatesManager = new GameStatesManager(gameStatesFabric);
+    }
+
+    @Test
+    public void testInitialState() {
+        GameBaseState expectedState = settingsState;
+
+        GameBaseState actualState = gameStatesManager.currentState();
+
+        assertEquals(expectedState, actualState);
+    }
+
+    @Test
+    public void testSwitchState() {
+        GameBaseState expectedState1 = playingState;
+        GameBaseState expectedState2 = winState;
+        GameBaseState expectedState3 = loseState;
+        GameBaseState actualState;
+
+        gameStatesManager.switchState(GameStates.PLAYING);
+        actualState = gameStatesManager.currentState();
+        assertEquals(expectedState1, actualState);
+
+        gameStatesManager.switchState(GameStates.WIN);
+        actualState = gameStatesManager.currentState();
+        assertEquals(expectedState2, actualState);
+
+        gameStatesManager.switchState(GameStates.LOSE);
+        actualState = gameStatesManager.currentState();
+        assertEquals(expectedState3, actualState);
+    }
+
+    @Test
+    public void testStateHandleAndNextState() {
+        GameBaseState expectedState1 = playingState;
+        GameBaseState expectedState2 = winState;
+        GameBaseState actualState;
+
+        when(settingsState.nextState()).thenReturn(GameStates.PLAYING);
+        gameStatesManager.stateHandle(game);
+        verify(settingsState).handle(game);
+        verify(settingsState).nextState();
+        actualState = gameStatesManager.currentState();
+        assertEquals(expectedState1, actualState);
+
+        when(playingState.nextState()).thenReturn(GameStates.WIN);
+        gameStatesManager.stateHandle(game);
+        verify(playingState).handle(game);
+        verify(playingState).nextState();
+        actualState = gameStatesManager.currentState();
+        assertEquals(expectedState2, actualState);
+    }
+}
